70c37aa0cbff5f1f1d7748caeb741938839b25eb
Aravind <aravind@webileapps.com>
2023-09-15 13:17:11 +0530

    addition of pdfmake module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import * as xml2js from 'xml2js';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
@@ -30,6 +35,7 @@ const language = 'typescript';
 
 ### Blockquote
 > Blockquote to the max`;
+  xmlData: string ='';
 
   constructor() {}
 
@@ -42,9 +48,46 @@ const language = 'typescript';
       const file: File = event.target.files[0];
       this.currentFile = file;
       this.fileName = this.currentFile.name;
+      console.log(this.currentFile, "currentFile");
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        this.xmlData = e.target?.result as string;
+        this.convertXmlToPdf();
+      };
+      reader.readAsText(this.currentFile);
+
     } else {
       this.fileName = 'Select File';
     }
+  }
+  async convertXmlToPdf() {
+    if (!this.xmlData) {
+      // Handle the case where XML data is not available.
+      return;
+    }
+
+    try {
+      // Parse the XML data to a JavaScript object using xml2js.
+      const parser = new xml2js.Parser();
+      const jsonData = await parser.parseStringPromise(this.xmlData);
+
+      // Create a PDF document using pdfmake.
+      const docDefinition = {
+        content: [
+          {
+            text: 'XML to PDF Conversion Example',
+            style: 'header',
+          },
+          JSON.stringify(jsonData, null, 2), 
+        ],
+      };
+
+      pdfMake.createPdf(docDefinition).download("test.pdf");
+    } catch (error) {
+      console.error('Error converting XML to PDF:', error);
+    }
+
   }
 
   upload(): void {
